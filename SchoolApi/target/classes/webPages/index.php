597bb9f3a2f8c0cbb66ec3d97ee3084171e7af62
shashi dhar <shashidharsrivastav5@gmail.com>
2026-02-01 23:30:32 +0530

    Create successfully connection with my sql and write code in application.properties.

---
+++ SchoolApi/target/classes/webPages/index.php
@@ -0,0 +1,26 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h1>City College of Management Matiyari, Lucknow</h1>
+    </header>
+    <div class="col">
+        
+    </div>
+    <div class="col">
+
+    </div>
+    <div class="col">
+
+    </div>
+    <footer></footer>
+</body>
+</html>
